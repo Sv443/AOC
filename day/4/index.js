ed3fe2683a3551cfaf62c58eabfbeb23cc5d9437
Sven Fehler <svenfehler@sv443.net>
2021-12-04 14:27:58 +0100

    done with input parsing of day 4

--- day/4/index.js
+++ day/4/index.js
@@ -1,15 +1,24 @@
 const { readFile } = require("fs-extra");
 const { resolve } = require("path");
 const { colors } = require("svcorelib");
+const { createInterface  } = require("readline");
+const { createReadStream } = require("fs");
 
 const col = colors.fg;
 
 const inputFilePath = resolve("./day/4/input.txt");
 
 
+/**
+ * @typedef {object} ParsedInputFile
+ * @prop {number[]} numbers Random chosen bingo numbers
+ * @prop {number[][][]} boards 3D array that contains all boards across the first dimension. The boards themselves make up the other 2 dimensions.
+ */
+
+
 async function run()
 {
-    const inputRaw = (await readFile(inputFilePath)).toString();
+    const { numbers, boards } = readInput();
 
     console.log("\n\nCalculating result for part one...\n");
     const part1 = getPart1();
@@ -21,10 +30,66 @@ async function run()
     console.log(`Result #2: ${col.green}${part2}${col.rst}\n`);
 }
 
+/**
+ * @returns {ParsedInputFile}
+ */
+async function readInput()
+{
+    const rl = createInterface(createReadStream(inputFilePath));
+
+    /** @type {ParsedInputFile} */
+    const result = {
+        numbers: [],
+        boards: [],
+    };
+
+    let curBoardIdx = 0;
+    let curBoardLineAmt = 0;
+
+    let lineNbr = 0;
+    for await (const line of rl)
+    {
+        const trimmedLine = line.trim();
+
+        lineNbr++;
+
+        console.log(`${col.yellow}DBG/${lineNbr}:${col.rst} ${trimmedLine}`);
+
+        if(lineNbr === 1) // random bingo numbers
+            result.numbers = trimmedLine.split(/,/g).map(n => parseInt(n));
+        else if(lineNbr > 2)
+        {
+            if(!trimmedLine.match(/^.*\d.*$/))
+                continue;
+
+            if(!Array.isArray(result.boards[curBoardIdx]))
+                result.boards[curBoardIdx] = [];
+
+            const lineNbrs = trimmedLine.split(/\s+/g).map(n => parseInt(n));
+
+            result.boards[curBoardIdx].push(lineNbrs);
+
+            curBoardLineAmt++;
+
+            if(curBoardLineAmt === 5)
+            {
+                curBoardLineAmt = 0;
+                curBoardIdx++;
+            }
+        }
+    }
+
+    return result;
+}
+
+//#SECTION part 1
+
 function getPart1()
 {
     return;
 }
+
+//#SECTION part 2
 
 function getPart2()
 {
