410b0a6e23b7cde59a8bd210052e77a84de5a074
Sven <s.fehler@pbsnetwork.eu>
2021-12-05 08:26:00 +0100

    add template

---
+++ day/template/index.js
@@ -0,0 +1,20 @@
+const { resolve } = require("path");
+const { colors } = require("svcorelib");
+
+const col = colors.fg;
+
+const inputFilePath = resolve("./day/REPLACE_ME/input.txt");
+
+async function run()
+{
+
+}
+
+//#SECTION part 1
+
+function getPart1()
+{
+
+}
+
+(() => run())();
