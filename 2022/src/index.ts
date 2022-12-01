7b2284771d09c4ff87b87ba7e7fc15dc2bd1590e
Sven <s.fehler@pbsnetwork.eu>
2022-12-01 11:46:39 +0100

    finish '22 boilerplate

---
+++ 2022/src/index.ts
@@ -0,0 +1,47 @@
+import { pathExists, readdir } from "fs-extra";
+import k from "kleur";
+import { join, resolve } from "path";
+import prompt from "prompts";
+
+const daysPath = resolve("./src/days/");
+const outDaysPath = resolve("./out/src/days/");
+
+async function run() {
+    let runDayNum;
+
+    const dayArg = process.argv.find(v => v.match(/^\d+$/gm));
+
+    if(!dayArg) {
+        const days = await getDays();
+
+        const { runDay } = await prompt({
+            type: "select",
+            name: "runDay",
+            message: "Choose which day to run",
+            choices: days.map(p => ({ title: `Day ${p.day}`, value: p.day })),
+        });
+
+        runDayNum = runDay;
+    }
+    else
+        runDayNum = parseInt(dayArg);
+
+    const importPath = join(outDaysPath, `/${runDayNum}/index.js`);
+    if(!(await pathExists(importPath))) {
+        console.error(k.red(`Couldn't run day ${runDayNum}:\n`) + `File '${importPath}' doesn't exist\n`);
+        process.exit(1);
+    }
+
+    console.log("\n");
+    await import(importPath);
+}
+
+async function getDays() {
+    return (await readdir(daysPath))
+        .sort((a, b) => a.localeCompare(b))
+        .map(folderName => ({
+            day: folderName,
+        }));
+}
+
+run();
