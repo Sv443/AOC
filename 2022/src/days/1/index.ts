16cfaf61175161a866bac8f4b8d97dd48aa9d6f3
Sven <s.fehler@pbsnetwork.eu>
2022-12-01 12:07:39 +0100

    finish day 1

--- 2022/src/days/1/index.ts
+++ 2022/src/days/1/index.ts
@@ -1,5 +1,21 @@
-function run() {
+import { readFile } from "fs-extra";
+import k from "kleur";
+import { resolve } from "path";
 
+async function run() {
+    const input = String(await readFile(resolve("./src/days/1/input.txt")));
+    const rawInventories = input.split(/\n\n/gm);
+    const inventories = rawInventories.map(invStr =>
+        invStr.split(/\n/gm)
+            .map(numCalories => parseInt(numCalories.trim()))
+            .filter(num => !isNaN(num)),
+    );
+    const added = inventories.map(inv => inv.reduce((a, c) => a + c, 0));
+    const sorted = added.sort((a, b) => a < b ? 1 : -1);
+
+    console.log(k.green("Top 1 amount: "), k.yellow(sorted.at(0) ?? "(error)"));
+    console.log(k.green("Top 3 amount: "), k.yellow(sorted.slice(0, 3).reduce((a, c) => a + c, 0)));
+    console.log();
 }
 
 run();
