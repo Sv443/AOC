fe1591df4ff02fdb653d677e4319f6cb29b2d5b9
Sven <s.fehler@pbsnetwork.eu>
2023-12-03 14:19:05 +0100

    feat: add template

---
+++ 2023/src/days/3/index.ts
@@ -0,0 +1,20 @@
+import k from "kleur";
+import { getInput } from "../../utils";
+
+const symbolRegex = /[^\d.]/gm;
+
+async function run() {
+  const input = await getInput(3, "");
+  const inputS = await getInput(3, "_s");
+
+  //#SECTION part 1
+  let sum1 = 0;
+  
+  //#SECTION part 2
+  // let sum2 = 0;
+
+  console.log(`${k.green("Part 1:")} ${k.yellow(sum1)}`);
+  // console.log(`${k.green("Part 2:")} ${k.yellow(sum2)}`);
+}
+
+run();
