be7db026810763e2b9d8f380e176c527885c6031
Sv443 <contact@sv443.net>
2024-12-02 16:40:05 +0100

    feat: 2024 day 1

--- 2024/src/days/1/index.ts
+++ 2024/src/days/1/index.ts
@@ -3,15 +3,47 @@ import { getInput } from "../../utils.js";
 
 async function run() {
   //#SECTION part 1
-  const input1 = await getInput(1);
+  let res1 = 0;
+  {
+    const input = await getInput(1);
 
+    const list1 = [] as number[];
+    const list2 = [] as number[];
+
+    for(const line of input) {
+      const [num1, num2] = line.split(/\s+/).map(Number);
+      typeof num1 === "number" && list1.push(num1);
+      typeof num2 === "number" && list2.push(num2);
+    }
+
+    list1.sort();
+    list2.sort();
+
+    for(let i = 0; i < list1.length; i++)
+      res1 += Math.abs(list1[i]! - list2[i]!);
+  }
 
   //#SECTION part 2
-  let input2 = await getInput(1);
+  let res2 = 0;
+  {
+    const input = await getInput(1);
+
+    const list1 = [] as number[];
+    const list2 = [] as number[];
+
+    for(const line of input) {
+      const [num1, num2] = line.split(/\s+/).map(Number);
+      typeof num1 === "number" && list1.push(num1);
+      typeof num2 === "number" && list2.push(num2);
+    }
+
+    for(const num of list1)
+      res2 += num * list2.filter(n => n === num).length;
+  }
 
 
-  console.log(`${k.green("Part 1:")} ${k.yellow(sum1)}`);
-  console.log(`${k.green("Part 2:")} ${k.yellow(sum2)}`);
+  console.log(`${k.green("Part 1:")} ${k.yellow(res1)}`);
+  console.log(`${k.green("Part 2:")} ${k.yellow(res2)}`);
 }
 
 run();
