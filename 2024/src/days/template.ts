e77b3e22f0a1cc5b6d29a5187a467b2e434320b6
Sv443 <contact@sv443.net>
2024-12-03 21:27:38 +0100

    feat: tsconfig paths & fix: template.ts exclusion error

--- 2024/src/days/template.ts
+++ 2024/src/days/template.ts
@@ -1,18 +1,18 @@
 import k from "kleur";
-import { getInput } from "../../utils.js";
+import { getInputLines } from "@/utils.js";
 
 async function run() {
   const aocDay = Number(import.meta.url.split("/").at(-2)! ?? 0);
-  const lines = await getInput(aocDay);
+  const lines = await getInputLines(aocDay);
   // const input = await getInput(aocDay, "_s");
 
-  //#SECTION part 1
+  //#region part 1
   let res1 = 0;
   {
 
   }
 
-  //#SECTION part 2
+  //#region part 2
   let res2 = 0;
   {
 
