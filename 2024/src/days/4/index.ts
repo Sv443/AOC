d43aa8780352146c3a5aaf0d8b03f566f24d89d9
Sv443 <contact@sv443.net>
2024-12-04 09:03:03 +0100

    feat: day 4 bp

---
+++ 2024/src/days/4/index.ts
@@ -0,0 +1,25 @@
+import k from "kleur";
+import { getInputLines } from "@/utils.js";
+
+async function run() {
+  const aocDay = Number(import.meta.url.split("/").at(-2) ?? 0);
+  const lines = await getInputLines(aocDay);
+
+  //#region part 1
+  let res1 = 0;
+  {
+
+  }
+
+  //#region part 2
+  let res2 = 0;
+  {
+
+  }
+
+  console.log(`${k.green("Part 1:")} ${k.yellow(res1)}`);
+  console.log(`${k.green("Part 2:")} ${k.yellow(res2)}`);
+  console.log();
+}
+
+run();
